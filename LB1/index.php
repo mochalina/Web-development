8ebc2b7b36b2e19b0ed35db72d2da81846e0d964
alina <80519419+mochalina@users.noreply.github.com>
2021-12-08 04:10:28 +0500

    Add files via upload
    Отредактировала index.php для корректного отображения на хостинге

--- LB1/index.php
+++ LB1/index.php
@@ -5,22 +5,22 @@
              echo($d);
            ?>
 <ul>
-	<li><a href="http://php-Mochalina/WWW/lab-1-1.php">Упр. 1-2</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.php">Упр. 1-3</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-3.php">Упр. 1-4</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-5.php">Сам. работа 1 - Вариант 5</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-5.1.php">Сам. работа 1 - Вариант 9</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.1.php">Упр. 1-6</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.2.php">Упр. 1-7</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.3.php">Упр. 1-8</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.4.php">Упр. 1-9</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.5.php">Упр. 1-10</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.6.php">Упр. 1-11</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.7.php">Упр. 1-12</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.8.php">Упр. 1-13</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.9.php">Упр. 1-14</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.10.php">Сам. работа 2 - Задача 1-2</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.11.php">Сам. работа 2 - Задача 1-3 - Вариант 3</a></li>
-	<li><a href="http://php-Mochalina/WWW/lab-1-2.11.1.php">Сам. работа 2 - Задача 1-3 - Вариант 8</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-1.php">Упр. 1-2</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.php">Упр. 1-3</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-3.php">Упр. 1-4</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-5.php">Сам. работа 1 - Вариант 5</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-5.1.php">Сам. работа 1 - Вариант 9</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.1.php">Упр. 1-6</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.2.php">Упр. 1-7</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.3.php">Упр. 1-8</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.4.php">Упр. 1-9</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.5.php">Упр. 1-10</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.6.php">Упр. 1-11</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.7.php">Упр. 1-12</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.8.php">Упр. 1-13</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.9.php">Упр. 1-14</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.10.php">Сам. работа 2 - Задача 1-2</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.11.php">Сам. работа 2 - Задача 1-3 - Вариант 3</a></li>
+	<li><a href="http://a0607404.xsph.ru/lab-1-2.11.1.php">Сам. работа 2 - Задача 1-3 - Вариант 8</a></li>
 
 </ul>	    				       
